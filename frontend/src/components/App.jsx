62a96489799db4d236a19aae354a74bb696c98af
Eero Kiili <eero.kiili@gmail.com>
2025-01-16 17:19:44 +0200

    Updated App component to work with Quiz

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,14 +2,13 @@ import { useState, useEffect } from 'react'
 import '../stylesheets/App.css'
 import { fetchWords } from './ApiConnections/FetchWords'
 import { addWord } from './ApiConnections/AddWord'
-import { updateStatus } from './ApiConnections/UpdateStatus'
 import { AdminPage } from './AdminOnly/AdminPage'
+import { Quiz } from './Features/Quiz/Quiz'
 
 const App = () => {
     const [words, setWords] = useState([])
     const [finnish, setFinnish] = useState('')
     const [english, setEnglish] = useState('')
-    const [translations, setTranslations] = useState({})
 
     // Fetch words list on component mount
     useEffect(() => {
@@ -33,38 +32,6 @@ const App = () => {
         fetchWords(setWords)
     }
 
-    // Checking the users answer
-    const checkTranslation = async (word) => {
-        const translation = translations[word.id]
-        // If correct, update the status to "learned"¨
-        if (translation.toLowerCase() === word.english_version.toLowerCase()) {
-
-            // Call updateStatus with word.id and status 1 (learned)
-            await updateStatus(word.id, 1)
-            alert('Correct translation!')
-
-            // Refresh the words list
-            fetchWords(setWords)
-
-        } else {
-            alert('Incorrect translation! Try again.')
-
-            // Reset the translation input field
-            setTranslations({
-                ...translations,
-                [word.id]: ''
-            })
-        }
-    }
-
-    // Update the state with the users translations
-    const translationHandler = (e, id) => {
-        setTranslations({
-            ...translations,
-            [id]: e.target.value
-        })
-    }
-
     // Props for the admin page
     const adminProps = {
         setWords,
@@ -73,8 +40,7 @@ const App = () => {
         setFinnish,
         english,
         setEnglish,
-        handleAddWord,
-        setTranslations
+        handleAddWord
     }
 
     return (
@@ -84,40 +50,7 @@ const App = () => {
 
             {/* Admin page */}
             <AdminPage {...adminProps} />
-
-            <div className="words-list-wrapper">
-                <h2>Words List</h2>
-                <ul className='words-list'>
-                    {words.length === 0 ? (<li>No words found.</li>)
-                        :
-                        (words.map((word) => (
-                            <div key={word.id}>
-                                <li>
-                                    <strong>{word.finnish_version}:</strong>
-                                </li>
-
-                                {/* If the word is learned (status is 1), display the correct translation */}
-                                {word.status === 1 ? (
-                                    <p className='correct-translation'>
-                                        <strong>Correct Translation:</strong> {word.english_version}
-                                    </p>
-                                ) : (
-                                    <>
-                                        <input className='translation-input'
-                                            type="text"
-                                            placeholder="Type the English translation"
-                                            value={translations[word.id] || ""}
-                                            onChange={(e) => translationHandler(e, word.id)}
-                                        />
-                                        <button className='check-button btn'
-                                            onClick={() => checkTranslation(word)}>Check answer</button>
-                                    </>
-                                )}
-                            </div>
-                        ))
-                        )}
-                </ul>
-            </div>
+            <Quiz words={words} />
         </div>
     )
 }
